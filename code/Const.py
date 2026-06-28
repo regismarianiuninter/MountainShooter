0a2708d0afe4e152bd4f52225666c60bdf9ba628
Reginaldo Mariani <5180656@alunouninter.com>
2026-06-28 16:04:16 -0300

    Adicionada pasta Asset Criado arquivo Const.py Criada tela inicial com música e menus (texto)

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
+#C
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 253, 85)
+
+# M
+
+MENU_OPTION = ('NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE',
+                'EXIT')
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
